1f0d59c4e3810ef846764d2dacc20dfd084e2b50
Kazi Imtiaj Ahammed <imtiaj5495@gmail.com>
2023-03-12 18:44:38 +0600

    first commit

---
+++ app/Http/Controllers/Admin/LoginController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    use AuthenticatesUsers;
+
+    public function __invoke(Request $request)
+    {
+        return view('admin.auth.login');
+    }
+
+    /**
+     * Where to redirect admins after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/admin';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest:admin')->except('logout');
+    }
+
+
+    public function showLoginForm()
+    {
+        return view('admin.auth.login');
+    }
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function login(Request $request)
+{
+    $this->validate($request, [
+        'email'   => 'required|email',
+        'password' => 'required|min:6'
+    ]);
+    if (Auth::guard('admin')->attempt([
+        'email' => $request->email,
+        'password' => $request->password
+    ], $request->get('remember'))) {
+        return redirect()->intended(route('admin.dashboard'));
+    }
+    return back()->withInput($request->only('email', 'remember'));
+}
+
+public function logout(Request $request)
+{
+    Auth::guard('admin')->logout();
+    $request->session()->invalidate();
+    return redirect()->route('admin.login');
+}
+
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Database\Seeders\AdminsTableSeeder;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(AdminsTableSeeder::class);
+    }
+}
